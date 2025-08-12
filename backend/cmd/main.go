ec4fc7c6e0e97bea41c8d5a65010a0de084a0d5a
Harry <HarryWilliam.sh0109@gmail.com>
2025-08-12 01:13:30 -0400

    Initialize monorepo or split repos (frontend/, backend/)

---
+++ backend/cmd/main.go
@@ -0,0 +1,14 @@
+package main
+
+// "github.com/ajonesb/user-management/backend/internal/controllers"
+// "github.com/ajonesb/user-management/backend/internal/middleware"
+// "github.com/ajonesb/user-management/backend/internal/repositories"
+// "github.com/ajonesb/user-management/backend/internal/services"
+// "github.com/ajonesb/user-management/backend/pkg/config"
+// "github.com/ajonesb/user-management/backend/pkg/database"
+// "github.com/gin-contrib/cors"
+// "github.com/gin-gonic/gin"
+
+func main() {
+
+}
